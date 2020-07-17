00747d68d219dfd9aa863b09411a7c81aec63d42
Michael Thomas <michael.thomas@renesas.com>
2020-07-17 12:26:38 +0000

    more cleanup

--- bl2/ext/mcuboot/bl2_main.c
+++ bl2/ext/mcuboot/bl2_main.c
@@ -18,8 +18,7 @@
 #include <assert.h>
 #include "bl2_util.h"
 #include "target.h"
-// #include "tfm_hal_device_header.h"
-#include "bsp_api.h"
+#include "tfm_hal_device_header.h"
 #include "Driver_Flash.h"
 #include "mbedtls/memory_buffer_alloc.h"
 #include "bootutil/bootutil_log.h"
@@ -29,7 +28,6 @@
 #include "boot_record.h"
 #include "security_cnt.h"
 #include "boot_hal.h"
-#include "memory_buffer_alloc.h"
 #if MCUBOOT_LOG_LEVEL > MCUBOOT_LOG_LEVEL_OFF
 #include "uart_stdout.h"
 #endif
